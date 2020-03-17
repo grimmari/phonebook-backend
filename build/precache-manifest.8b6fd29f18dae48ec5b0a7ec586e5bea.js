e303003ab8b169e50a9394ecfdae65c55d8499de
grimmari <arip.grimm@gmail.com>
2020-03-17 21:51:11 +0200

    make it best

---
+++ build/precache-manifest.8b6fd29f18dae48ec5b0a7ec586e5bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35824b22a467db6ddcab2b5f58b282a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4ba1804d5d0f5759c6",
+    "url": "/static/css/main.efd4d301.chunk.css"
+  },
+  {
+    "revision": "5dfdef0a83e89ca6cd65",
+    "url": "/static/js/2.230301dd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.230301dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4ba1804d5d0f5759c6",
+    "url": "/static/js/main.cb417576.chunk.js"
+  },
+  {
+    "revision": "ad9c1ae9b2cdeb3f7618",
+    "url": "/static/js/runtime-main.584b4e6a.js"
+  }
+]);
---
+++ mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+    `mongodb+srv://grimmari-atlas:${password}@cluster0-1actz.mongodb.net/phonebook-app?retryWrites=true`
+//mongodb+srv://grimmari-atlas:<password>@cluster0-1actz.mongodb.net/test?retryWrites=true&w=majority
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+if (process.argv.length === 3) {
+    const personSchema = new mongoose.Schema({
+        name: String,
+        number: String,
+
+    })
+console.log('phonebook:')
+    const Person = mongoose.model('Person', personSchema)
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+      })
+}
+
+if (process.argv.length === 5) {
+    const personSchema = new mongoose.Schema({
+        name: String,
+        number: String,
+
+    })
+
+    const Person = mongoose.model('Person', personSchema)
+
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+
+    person.save().then(response => {
+
+        console.log(`added ${response.name} number ${response.number}`)
+        mongoose.connection.close();
+    })
+}
+
+//ZSsQDzq5YqGghf0W
